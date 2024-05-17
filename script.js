ceb27d45dafd183d33acde792ede0d92b9fedb63
Nicolas Falcão <106965763+nicolasvfalcao@users.noreply.github.com>
2024-05-16 23:33:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+const form = document.querySelector("form");
+form.addEventListener("submit", function (evento) {
+  evento.preventDefault()
+  calcular()
+})
+
+const numero1 = document.getElementById("numero1");
+const numero2 = document.getElementById("numero2");
+const opcoes = document.getElementById("opcoes");
+
+const resultadoCalculo = document.getElementById("resultado-calculo")
+
+function calcular(){
+  // debugger
+  switch(opcoes.value){
+    case "soma":
+      soma()
+      break;
+    case "subtracao":
+      subtracao()
+      break;
+    case "divisao":
+      divisao()
+      break;
+    case "multiplicacao":
+      multiplicacao()
+      break
+  }
+}
+
+function soma(){
+  let resultado = Number(numero1.value.replace(",", ".")) + Number(numero2.value.replace(",", "."));
+  resultadoCalculo.innerText = `O resultado da soma é ${resultado}`
+}
+
+function subtracao(){
+  let resultado = Number(numero1.value.replace(",", ".")) - Number(numero2.value);
+  resultadoCalculo.innerText = `O resultado da subtração é ${resultado}`
+}
+
+function divisao(){
+  let resultado = Number(numero1.value) / Number(numero2.value);
+  resultadoCalculo.innerText = `O resultado da divisão é ${resultado}`
+}
+
+function multiplicacao(){
+  let resultado = Number(numero1.value) * Number(numero2.value);
+  resultadoCalculo.innerText = `O resultado da multiplicação é ${resultado}`
+}
